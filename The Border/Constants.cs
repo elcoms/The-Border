e841272b34a8ed0f3b88aae688e9fc83cdf62f3a
Elcoms <elcomsortsort@gmail.com>
2019-11-19 16:16:07 +0700

    Add Door class

--- The Border/Constants.cs
+++ The Border/Constants.cs
@@ -6,17 +6,17 @@
         public const string DUNGEON_FILE = "..\\..\\data\\dungeon.txt";
 
         // char
-        public const char PLAYER = 'O';
-        public const char ENEMY = 'E';
-        public const char ENEMY_DEAD = 'X';
-        public const char WALL = '█';
-        public const char SPACE = ' ';
-        public const char OUT_OF_BOUNDS = 'B';
+        public const char PLAYER        = 'O';
+        public const char ENEMY         = 'E';
+        public const char ENEMY_DEAD    = 'X';
+        public const char WALL          = '█';
+        public const char SPACE         = ' ';
+        public const char DOOR          = '▌';
 
         // world size
-        public const int WORLD_WIDTH = 100;
-        public const int WORLD_HEIGHT = 100;
-        public const int CAM_WIDTH = 100;
-        public const int CAM_HEIGHT = 100;
+        public const int WORLD_WIDTH    = 100;
+        public const int WORLD_HEIGHT   = 100;
+        public const int CAM_WIDTH      = 100;
+        public const int CAM_HEIGHT     = 100;
     }
 }
---
+++ The Border/scripts/Door.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Border.scripts
+{
+    class Door
+    {
+        ConsoleColor color = ConsoleColor.DarkRed;
+
+        int x = 1, y = 1;
+        string name = "The Door";
+        bool unlocked = false;
+
+        public Door (int xPos, int yPos, string givenName, ConsoleColor givenColor)
+        {
+            x = xPos;
+            y = yPos;
+            name = givenName;
+            color = givenColor;
+        }
+
+        public void Render()
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(x, y);
+            Console.Write(Constants.DOOR);
+        }
+
+        public OnCollision(Player player)
+        {
+            // Check for key
+            // if key, Log: && unlocked = true;
+            // else Log:
+        }
+    }
+}
